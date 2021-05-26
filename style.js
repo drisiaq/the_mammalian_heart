f05f152654d9698a426005df9e61adadd6b56b19
drisiaq <42673220+drisiaq@users.noreply.github.com>
2021-05-26 14:01:30 +0100

    Update style.js

--- style.js
+++ style.js
@@ -5,3 +5,7 @@ function log(){
 function sub(){
         // alert("Please, fill the form")
     } 
+
+document.getElementById("contact").onclick = function(){
+    alert(We will get in touch with you soon!)
+}
